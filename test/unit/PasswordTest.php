7c5d921379d046ab685210610cb34fde08a1fe15
Scott <scott@paragonie.com>
2015-10-25 23:54:40 -0400

    Update unit test too.

--- test/unit/PasswordTest.php
+++ test/unit/PasswordTest.php
@@ -1,6 +1,6 @@
 <?php
 use \ParagonIE\Halite\Password;
-use \ParagonIE\Halite\Symmetric\SecretKey as SymmetricKey;
+use ParagonIE\Halite\Symmetric\EncryptionKey;
 
 /**
  * @backupGlobals disabled
@@ -10,7 +10,7 @@ class PasswordTest extends PHPUnit_Framework_TestCase
 {
     public function testEncrypt()
     {
-        $key = new SymmetricKey(\str_repeat('A', 32));
+        $key = new EncryptionKey(\str_repeat('A', 32));
         
         $hash = Password::hash('test password', $key);
         $this->assertTrue(is_string($hash));

f0ef5546bc3eeeb61bb970ae17ed182da14719f5
steffi <stefanie@reneschmidt.de>
2015-11-28 13:59:44 +0100

    added util test case

---
+++ test/unit/UtilTest.php
@@ -0,0 +1,30 @@
+<?php
+use ParagonIE\Halite\Alerts\CannotPerformOperation;
+use ParagonIE\Halite\Util;
+
+/**
+ * Halite Util test case
+ *
+ * @category   HaliteTest
+ * @package    Halite
+ * @author     Stefanie Schmidt <stefanie@reneschmidt.de>
+ * @license    http://opensource.org/licenses/GPL-3.0 GPL 3
+ * @link       https://paragonie.com/project/halite
+ */
+class UtilTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * test safeStrLen() with illegal parameter. We expect to see an exception
+     * @return void
+     * @throws CannotPerformOperation
+     */
+    public function testSafeStrlen()
+    {
+        $this->setExpectedException('ParagonIE\Halite\Alerts\CannotPerformOperation');
+
+        $teststring = []; // is not a string, will provoke a warning
+
+        //suppress php warning
+        @Util::safeStrlen($teststring);
+    }
+}

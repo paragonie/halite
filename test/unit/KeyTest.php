8759eae41547d6e76fff0a2a36185250bb289fec
Paragon Initiative Enterprises <security@paragonie.com>
2016-05-07 15:34:22 -0400

    Skipping this test. Will re-enable it as needed, but it will slow Travis CI down.

--- test/unit/KeyTest.php
+++ test/unit/KeyTest.php
@@ -341,9 +341,8 @@ class KeyTest extends PHPUnit_Framework_TestCase
 
     public function testKeyLevels()
     {
-        /*
         return $this->markTestSkipped('This is a very slow test. Feel free to enable it to verify correctness.');
-        */
+
         $key = KeyFactory::deriveEncryptionKey(
             'apple',
             "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f".

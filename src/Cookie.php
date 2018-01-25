ad78c6553c088ad46fb38d0d1349d228c4a9d50d
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-25 15:48:39 -0500

    Update Cookie.php

--- src/Cookie.php
+++ src/Cookie.php
@@ -7,17 +7,15 @@ use \ParagonIE\ConstantTime\{
     Binary,
     Hex
 };
-use \ParagonIE\Halite\Alerts\{
-    CannotPerformOperation,
-    InvalidDigestLength,
-    InvalidMessage,
-    InvalidSignature,
-    InvalidType
-};
-use \ParagonIE\Halite\Symmetric\{
-    Config as SymmetricConfig,
-    Crypto,
-    EncryptionKey
+use \ParagonIE\Halite\{
+    Alerts\CannotPerformOperation,
+    Alerts\InvalidDigestLength,
+    Alerts\InvalidMessage,
+    Alerts\InvalidSignature,
+    Alerts\InvalidType,
+    Symmetric\Config as SymmetricConfig,
+    Symmetric\Crypto,
+    Symmetric\EncryptionKey
 };
 
 /**

d678d155bb73a7b0f1b837dd31d48230833b91eb
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-25 02:12:59 -0500

    Update NodeTest.php

--- test/unit/Structure/NodeTest.php
+++ test/unit/Structure/NodeTest.php
@@ -8,11 +8,11 @@ use PHPUnit\Framework\TestCase;
  * @backupGlobals disabled
  * @backupStaticAttributes disabled
  */
-class NodeTest extends TestCase
+final class NodeTest extends TestCase
 {
     public function testHash()
     {
-        $stringData = \random_bytes(32);
+        $stringData = random_bytes(32);
         $hash = sodium_crypto_generichash($stringData);
         $node = new Node($stringData);
 

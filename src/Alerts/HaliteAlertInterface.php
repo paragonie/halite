abb227cd2b7b33b7cb2fd4bf873fae7e4c2b070d
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-12 23:17:57 -0400

    Update HaliteAlertInterface.php

--- src/Alerts/HaliteAlertInterface.php
+++ src/Alerts/HaliteAlertInterface.php
@@ -1,1 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Interface HaliteAlertInterface
+ *
+ * @package ParagonIE\Halite\Alerts
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
+namespace ParagonIE\Halite\Alerts;
+
+/**
+ * HaliteAlertInterface
+ */
+interface HaliteAlertInterface
+{
+}
